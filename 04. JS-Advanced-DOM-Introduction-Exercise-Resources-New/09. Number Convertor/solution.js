1e882367674ae3191803f529db635dabc5c4dd49
Nikoleta Zlateva <87802876+Nikoletazl@users.noreply.github.com>
2022-06-14 13:22:03 +0300

    finished the rest problems

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/09. Number Convertor/solution.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/09. Number Convertor/solution.js
@@ -1,4 +1,27 @@
 function solve() {
+    let binary = document.createElement('option')
+    binary.value = 'binary'
+    binary.innerHTML = 'Binary'
+    let hexadecimal = document.createElement('option')
+    hexadecimal.value = 'hexadecimal'
+    hexadecimal.innerHTML = 'Hexadecimal'
+    let options = document.getElementById('selectMenuTo')
+    options.appendChild(binary)
+    options.appendChild(hexadecimal)
 
-    //TODO...
+    let button = document.getElementsByTagName('button')[0].addEventListener('click', onClick)
+
+    function onClick () {
+        let output = 0
+        let number = Number(document.getElementById('input').value)
+        let selectedOption = document.querySelector('#selectMenuTo option:checked').value
+
+        if(selectedOption == 'binary'){
+            output = number.toString(2)
+        }else {
+            output = number.toString(16).toUpperCase()
+        }
+        document.getElementById('result').value = output
+    }
+
 }
